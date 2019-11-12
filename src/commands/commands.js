1db1c425e8126d9c4f71dadab2f2c1f82aad828a
Sam Parkinson <parkinsonsp42@gmail.com>
2019-11-11 21:59:11 -0500

    Implement args and command splitters

--- src/commands/commands.js
+++ src/commands/commands.js
@@ -1,5 +1,15 @@
+require('dotenv').config();
+const { prefix } = require('../config');
+
 function commands(msg) {
-  console.log('This will send a command');
+  const { args, command } = isolate(msg)
+  console.log(`Command: ${command}\nArguments: ${args}`); 
+}
+
+function isolate(msg) {
+  const args = msg.content.slice(prefix.length).split(' ');
+  const command = args.shift().toLowerCase();  
+  return { args, command };
 }
 
 module.exports = commands;
