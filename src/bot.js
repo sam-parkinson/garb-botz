30b853f6835faf29f5cb2fe76abef7beaec6a7cc
Sam Parkinson <parkinsonsp42@gmail.com>
2019-12-13 12:47:36 -0500

    Update bot.js to include file system logic

--- src/bot.js
+++ src/bot.js
@@ -1,10 +1,18 @@
 require('dotenv').config();
+const fs = require('fs');
 const Discord = require('discord.js');
 const { DISCORD_TOKEN, prefix } = require('./config');
-const commands = require('./commands/commands');
 const herald = require('./herald/herald');
 
 const client = new Discord.Client();
+client.commands = new Discord.Collection();
+
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+  const command = require(`./commands/${file}`);
+  client.commands.set(command.name, command);
+}
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}`);
@@ -18,7 +26,17 @@ client.on('message', msg => {
   if (msg.author.bot) {
     return;
   } else if (msg.content.startsWith(prefix)) {
-    commands(msg);
+    const args = msg.content.slice(prefix.length).split(/ +/);
+    const command = args.shift().toLowerCase();
+
+    if (!client.commands.has(command)) return;
+
+    try {
+      cilent.commands.get(command).execute(message, args);
+    } catch (err) {
+      console.error(err);
+      message.reply('There was an error trying to execute that command')
+    }
   } else { 
     herald(msg);
   }
