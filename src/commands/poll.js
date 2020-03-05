6b1c46343f82e50ea0a1e21ba3d2f0508d3bbe56
Sam Parkinson <parkinsonsp42@gmail.com>
2020-03-05 10:42:57 -0500

    Add some poll pseudo

--- src/commands/poll.js
+++ src/commands/poll.js
@@ -13,3 +13,11 @@ module.exports = {
     return msg.channel.send(`Poll function is still under construction`)
   }
 }
+
+// how will this work?
+// DM user for question, then...
+// for each option, DM user who requested the poll, await response
+// after for loop closes, post poll in original channel
+// set timeout object for hours 
+// after hours closes, count responses on original message
+// post results as percentages 
