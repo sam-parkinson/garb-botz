966ee5b94aad44617698671d7fc48fc143dfdc51
Sam Parkinson <parkinsonsp42@gmail.com>
2020-03-05 15:33:47 -0500

    Begin poll request process

--- src/commands/poll.js
+++ src/commands/poll.js
@@ -10,7 +10,21 @@ module.exports = {
     if (!args.length) {
       return msg.reply(`please provide the number of options and the time in hours for which the poll will run`);
     }
-    return msg.channel.send(`Poll function is still under construction`)
+
+    const optionNumb = args[0];
+    const runtime = args[1];
+
+    // add logic to get question from user
+
+    // array stores responses from DMs, I think
+    const options = [];
+
+    for (let i = 0; i < options; i++) {
+      msg.author.send(`Option ${i + 1}: (option)`);
+      // add logic to get responses from DMs, push to options array
+    }
+
+    return msg.channel.send(`Options: ${optionNumb}, runtime in hours: ${runtime}`);
   }
 }
 
