e6711f66d97fb3bccd79ddd4c9681e378f0df2c6
Sam Parkinson <parkinsonsp42@gmail.com>
2020-04-30 17:47:56 -0400

    Fleshed out async logic

--- src/commands/poll.js
+++ src/commands/poll.js
@@ -6,36 +6,46 @@ module.exports = {
   aliases: ['survey'],
   usage: '[number of options] [runtime in hours]',
   cooldown: 10,
-  execute(msg, args) {
+  async execute(msg, args) {
     if (!args.length) {
       return msg.reply(`please provide the number of options and the time in hours for which the poll will run`);
     }
 
-    const optionNumb = args[0];
+    const n = args[0];
     const runtime = args[1];
+  
+    const filter = response => response.content
 
-    // add logic to get question from user
+    let options = await responder(msg, filter, n);
+    console.log('Second: ' + options);
 
-    // array stores responses from DMs, I think
-    const options = [];
+    // const options = Promise.resolve(responder(msg, filter, n));
 
-    for (let i = 0; i < optionNumb; i++) {
-      // potentially factor this out into own function?
-      msg.author.send(`Option ${i + 1}:`)
-        .then(() => {
-          msg.channel.awaitMessages(filter, { maxMatches: 1 })
-        })
-        // need to wait for async function to finish before sending next option
-      // add logic to get responses from DMs, push to options array
+    // console.log('Second: '+ options);
 
-      // should use collection logic
-    }
-
-    return msg.channel.send(`Options: ${options}, runtime in hours: ${runtime}`);
+    // return msg.channel.send(`Options: ${options}, runtime in hours: ${runtime}`);
   }
 }
 
-const filter = response => response;
+const responder = async (msg, filter, n) => {
+  const arr = [];
+  return msg.author.send(`Please provide ${n} options`)
+  .then((reply) => {
+    return reply.channel.awaitMessages(filter, {
+      max: n,
+      time: 30000,
+      errors: ['time'],
+    })
+  }).then((collected) => {
+    for (const value of collected.values()) {
+      arr.push(value.content);
+    }
+    console.log('First: '+ arr);
+    return arr;
+  }).catch(err => 
+    console.error(`An error occured: ${err}`)
+  );
+}
 
 // how will this work?
 // DM user for question, then...
