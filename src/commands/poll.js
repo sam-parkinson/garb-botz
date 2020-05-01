46fb4ed8cb1f36c62bd07c503a9d8372fda4efb5
Sam Parkinson <parkinsonsp42@gmail.com>
2020-05-01 18:03:44 -0400

    Add beginnings of poll making function

--- src/commands/poll.js
+++ src/commands/poll.js
@@ -13,23 +13,33 @@ module.exports = {
 
     const n = args[0];
     const runtime = args[1];
-  
+
+    if (n > 26) {
+      return msg.reply(`Poll currently does not support more than twenty-six options.`);
+    }
+
+    // maybe add some restriction on how long the poll runs?
+
     const filter = response => response.content
 
-    let options = await responder(msg, filter, n);
-    console.log('Second: ' + options);
+    let question = await prompter(msg, filter, 1, true);
+    let options = await prompter(msg, filter, n);
 
-    // const options = Promise.resolve(responder(msg, filter, n));
+    makePoll(msg, question, options, runtime);
 
-    // console.log('Second: '+ options);
-
-    // return msg.channel.send(`Options: ${options}, runtime in hours: ${runtime}`);
+    /* return msg.channel.send( 
+      ':joy:: word\n' +
+      `Question: ${question[0]}\n` + 
+      `Options: ${options.join(' ')}\n` +
+      `Runtime in hours: ${runtime}`
+    ); */
   }
 }
 
-const responder = async (msg, filter, n) => {
+const prompter = async (msg, filter, n, question = false) => {
+  const prompt = question ? `Please provide the poll question` : `Please provide ${n} options`;
   const arr = [];
-  return msg.author.send(`Please provide ${n} options`)
+  return msg.author.send(prompt)
   .then((reply) => {
     return reply.channel.awaitMessages(filter, {
       max: n,
@@ -40,17 +50,32 @@ const responder = async (msg, filter, n) => {
     for (const value of collected.values()) {
       arr.push(value.content);
     }
-    console.log('First: '+ arr);
     return arr;
   }).catch(err => 
     console.error(`An error occured: ${err}`)
   );
 }
 
-// how will this work?
-// DM user for question, then...
-// for each option, DM user who requested the poll, await response
-// after for loop closes, post poll in original channel
+const makePoll = (msg, question, options, runtime) => {
+  const data = [];
+
+  data.push('**NEW POLL**');
+  data.push(question[0]);
+
+  for (let i = 0; i < options.length; i++) {
+    // this works!
+    const ltr = String.fromCodePoint(0x1F1E6 + i);
+    data.push(`\n ${ltr}: ${options[i]}`);
+  }
+
+  data.push(`\nThis poll will run for ${runtime} hours.`);
+  data.push('\nReact to this message with your answer!');
+
+  return msg.channel.send(data, { split: true });
+}
+
+
+// build message object based on poll options and question
 // set timeout object for hours 
 // after hours closes, count responses on original message
 // post results as percentages 
